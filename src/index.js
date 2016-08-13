ba46d0b03ebac341956055dbe80e1ea9b875f7d3
Lukas Neicelis <l.neicelis@gmail.com>
2016-08-13 19:32:58 +0300

    Exporting additional createAPI method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import Rx from 'rx';
 import observableAPIFactory from './observable-api';
 import jQuery from './adapters/jquery';
 
-export default observableAPIFactory(Rx);
+const createObservableAPI = observableAPIFactory(Rx);
+
+export default createObservableAPI;
+
+export const createAPI = createObservableAPI;
 
 export const jQueryAdapter = jQuery;
